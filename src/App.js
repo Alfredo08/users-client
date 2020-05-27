1ce5342912723390f72364db42e339a25ba4aadf
Alfredo Salazar <>
2020-05-27 07:56:06 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component{
+  constructor( props ){
+    super( props );
+    this.state = {
+      errorMessage : "",
+      apiUrl : "http://localhost:8080/api"
+    }
+  }
+
+  handleLogin = ( event ) => {
+    event.preventDefault();
+    
+    const userEmail = event.currentTarget.userEmail.value;
+    const userPassword = event.currentTarget.userPassword.value;
+
+    const data = {
+      email : userEmail,
+      password : userPassword
+    };
+
+    const url = `${this.state.apiUrl}/users/login`;
+    
+    const settings = {
+      method : 'POST',
+      headers : {
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify( data )
+    };
+
+    fetch( url, settings )
+      .then( response => {
+        if( response.ok ){
+          return response.json();
+        }
+        throw new Error( response.statusText );
+      })
+      .then( responseJSON => {
+        localStorage.setItem( 'token', responseJSON.token );
+        this.props.history.push( '/home-page' );
+      })
+      .catch( err => {
+        this.setState({
+          errorMessage : err.message
+        });
+      });
+  }
+
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.handleLogin}>
+          <div>
+            <label htmlFor="userEmail">
+              Email : 
+            </label>
+            <input type="text" name="userEmail" id="userEmail" />
+          </div>
+          <div>
+            <label htmlFor="userPassword">
+              Password : 
+            </label>
+            <input type="password" name="userPassword" id="userPassword" />
+          </div>
+          <button type="submit">
+            Login
+          </button>
+        </form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.errorMessage}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Home from './Home';
+import Profile from './Profile';
+import NoRouteMatch from './NoRouteMatch';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/home-page" component={Home} />
+      <Route exact path="/profile-page" component={Profile} />
+      <Route component={NoRouteMatch} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
